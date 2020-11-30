9be488afd8f290b7d69cb58c85ecaaa9fc53b567
david cortesi <david.cortesi.097@gmail.com>
2020-11-30 15:47:09 +0100

    commit 1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const header = new Vue({
+  el:"header",
+  data:{
+    list:[
+      logo:"logo.png",
+      menu:{
+        "home",
+        "pages",
+        "program",
+        "tickets",
+        "speakers",
+        "papers",
+        "blog",
+        "shortcodes",
+        "|",
+      },
+       icon:"fas fa-search"
+    ],
+  },
+  methods:{},
+});
