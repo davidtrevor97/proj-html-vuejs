2c90787517d11397d8b98852b09c5e3076d3b9e3
david cortesi <david.cortesi.097@gmail.com>
2020-12-01 02:09:58 +0100

    commit 5

--- js/main.js
+++ js/main.js
@@ -29,3 +29,51 @@ const slider = new Vue({
   },
 
 });
+
+const timerContainer = new Vue({
+  el: ".timerContainer",
+  data:{
+    displayDays: 0,
+    displayHours: 0,
+    displayMinutes: 0,
+    displaySeconds: 0,
+  },
+  computed:{
+    seconds:() => 1000,
+    minutes(){
+      return this.seconds * 60
+    },
+    hours() {
+      return this.minutes * 60
+    },
+    days(){
+      return this.hours * 24
+    }
+  },
+  mounted(){
+    console.log("ciaooooo");
+    this.showTime();
+  },
+  methods:{
+    showTime(){
+      const timer = setInterval(() => {
+        const now = new Date();
+        const end = new Date(2020,12,25,0,0,0);
+        const passingTime = end.getTime() - now.getTime();
+        if (passingTime < 0) {
+          clearInterval(timer);
+          return;
+        }
+        const days = Math.floor(passingTime / this.days);
+        const hours = Math.floor((passingTime % this.days) / this.hours );
+        const minutes = Math.floor((passingTime % this.hours) / this.minutes );
+        const seconds = Math.floor((passingTime % this.minutes) / this.seconds);
+        this.displayMinutes = minutes < 10 ? "0" + minutes : minutes;
+        this.displaySeconds = seconds < 10 ? "0" + seconds : seconds;
+        this.displayHours = hours < 10 ? "0" + hours : hours;
+        this.displayDays = days < 10 ? "0" + days : days;
+
+      }, 1);
+    }
+  },
+});
